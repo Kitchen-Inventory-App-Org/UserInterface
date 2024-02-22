dea2345daf052178f4c64572cc8225a0d289072b
CamiJolly1 <cjolly3@msudenver.edu>
2024-02-22 14:24:32 -0700

    adding bryan's code

--- pantryprofileui/src/App.js
+++ pantryprofileui/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import the CSS file
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>Kitchen Table</h1>
+      </div>
+      <div className="container">
+        <input type="text" className="search-bar" placeholder="Search..." />
+        <div className="dropdown">
+          <button className="dropbtn">Inventory</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
